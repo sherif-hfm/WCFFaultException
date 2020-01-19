38a83d833de5d4754e313b939c9cbe83c827e832
Sherif <sherif_hfm@yahoo.com>
2020-01-19 12:41:20 +0300

    init

---
+++ ServiceTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using ServiceTest.Srv1Ref;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Srv1Ref.Service1Client srv = new Srv1Ref.Service1Client();
+                var result = srv.GetData(6);
+            }
+            catch (FaultException ex)
+            {
+                
+            }
+        }
+    }
+}
---
+++ WcfService1/Service1.svc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            if(value <= 5)
+            {
+                return string.Format("You entered: {0}", value.ToString());
+            }
+            else
+            {
+                var dtl = new ExceptionContract() { ErrMsg = "Err Msg" };               
+                //throw new FaultException(MessageFault.CreateFault(new FaultCode("Fault Code"), new FaultReason("Fault Reason"), dtl));
+                throw new FaultException<ExceptionContract>(new ExceptionContract() { ErrMsg = "grater than 5" }, new FaultReason("Fault Reason"));
+                //throw new FaultException<ExceptionContract>(new ExceptionContract() { ErrMsg = "grater than 5" }, "Reason", new FaultCode("Fault Code"));
+            }
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
